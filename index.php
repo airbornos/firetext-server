1e332f0de62838f1dac88c3c591aad2bcd68a866
Joshua Smith <thewanuki@gmail.com>
2015-12-03 15:36:05 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+if (isset($_POST) && !empty($_POST)) {
+	if($_POST['request']=='firetext_variables'){
+		// Firetext 0.5+
+		// Variable container
+		$firetextVariables = [];
+		
+		// Cloud services
+		$firetextVariables["services"] = [];
+		
+		// Dropbox config
+		$firetextVariables["services"]["dropbox"] = [
+			"authURL": "https://codexa.github.io/firetext/auth/dropbox/oauth2/",
+			"apiKey": "p8qpg4ai84mfayx"
+		];
+		
+		// Splunk config
+		$firetextVariables["services"]["splunk"] = [
+			"apiKey": ""
+		];
+		
+		// In-app urls
+		$firetextVariables["urls"] = [
+					[
+						"name": "rate",
+						"url": "https://marketplace.firefox.com/app/firetext/ratings/add",
+						"category": "app_actions"
+					],
+					[
+						"name": "support",
+						"url": "https://support.mozilla.com/",
+						"category": "help"
+					]
+				];
+		
+		// Notifications
+		$firetextVariables["notifications"] = [];
+		if($_POST["locale"] == "en-US") {
+			array_push($firetextVariables["notifications"],[
+				"name": "test",
+				"content": "want-feedback",
+				"repeat": 2
+			]);
+		}
+	
+		// Echo variables
+		header('Content-Type: application/json');
+		header('Access-Control-Allow-Origin: *');
+		echo json_encode($firetextVariables);
+		exit;		
+	} else if($_POST['request']=='urls') {
+		// Firetext 0.4-
+		// Dropbox config
+		$dropboxURL = 'https://codexa.github.io/firetext/auth/dropbox/';
+		
+		// Create variables
+		$urls = array(
+					"about"=>"",
+					"credits"=>"",
+					"support"=>"",
+					"rate"=>"https://marketplace.firefox.com/app/firetext/ratings/add",
+					"dropboxAuth"=>$dropboxURL
+				);
+	
+		// Echo variables
+		header('Content-Type: application/json');
+		header('Access-Control-Allow-Origin: *');
+		echo json_encode($urls);
+		exit;
+	}
+}
